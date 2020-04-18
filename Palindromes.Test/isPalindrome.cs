0ec2fc1fbea9dadc37033ad0a0e641572f2f7c40
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-04-18 11:48:32 -0500

    tested the other cases in my test unit

--- Palindromes.Test/isPalindrome.cs
+++ Palindromes.Test/isPalindrome.cs
@@ -6,7 +6,7 @@ namespace Palindromes.Test
     public class isPalindrome
     {
         [Fact]
-        public void is_stars_palindrome()
+        public void is_test1_palindrome()
         {
             var palindrome = "stars";
             var rev = "srats";
@@ -15,6 +15,32 @@ namespace Palindromes.Test
 
             var actualResult = palidromeChecker.Check(palindrome, rev);
             Assert.Equal(expectedResulted,actualResult);
+
+        }
+
+        [Fact]
+        public void is_test2_palindrome()
+        {
+            var palindrome = "O, a kak Uwakov lil vo kawu kakao!";
+            var rev = "!oakak uwak ov lil vokawU kak a, O";
+            var expectedResulted = true;
+            var palidromeChecker = new PalindromeChecker();
+
+            var actualResult = palidromeChecker.Check(palindrome, rev);
+            Assert.Equal(expectedResulted, actualResult);
+
+        }
+
+        [Fact]
+        public void is_test3_palindrome()
+        {
+            var palindrome = "Some men interpret nine memos";
+            var rev = "somem enin terpretni nem emos";
+            var expectedResulted = true;
+            var palidromeChecker = new PalindromeChecker();
+
+            var actualResult = palidromeChecker.Check(palindrome, rev);
+            Assert.Equal(expectedResulted, actualResult);
 
         }
     }
